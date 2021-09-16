d2178b1edcfa388e174885d836cb41d1a6309bdb
LazarWang <xse_mail@163.com>
2021-09-16 18:01:57 +0800

    更改名称和图标

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  title: 'Vue Admin Template',
+  title: '波洛斯开放平台',
 
   /**
    * @type {boolean} true | false
--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -1,6 +1,6 @@
 import defaultSettings from '@/settings'
 
-const title = defaultSettings.title || 'Vue Admin Template'
+const title = defaultSettings.title || '波洛斯开放平台'
 
 export default function getPageTitle(pageTitle) {
   if (pageTitle) {
